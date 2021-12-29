1d88c4c4b791425c4d7cf6c31d67a3cd5489ee68
962n <tak.k1u2r2o4@gmail.com>
2021-12-30 00:19:48 +0900

    test

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# golangci & reviewdog実行のため各モジュールのパス(go.modが存在する場所)を探索し配列に格納する。ただし、protoは除外する。
+array=`find \`pwd\` | grep "/go.mod" | sed -e "s/\/go.mod//" `
+CURRENT=`echo \`pwd\``
+
+for a in $array; do
+  MOD_PATH=`echo $a`
+  cd $MOD_PATH
+  echo `pwd`
+  golangci-lint run -c ${CURRENT}/golangci.yaml ./... | reviewdog -f=golangci-lint  -diff="git diff FETCH_HEAD"
+done
